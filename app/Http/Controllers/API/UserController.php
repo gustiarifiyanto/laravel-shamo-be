f91f2053f18ceea58b16dbfb36aebe79fb1425c7
gustiarifiyanto <gusti.arifiyanto@gmail.com>
2025-10-21 00:19:27 +0700

    Menambahkan function update profil dan logout pada API

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -84,4 +84,25 @@ class UserController extends Controller
             ], 'Authentication Failed', 500);
         }
     }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success($request->user(), 'Data profile user berhasil diambil');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $data = $request->all();
+        $user = Auth::user();
+        $user->update($data);
+        
+        return ResponseFormatter::success($user, 'Profile updated');
+    }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken()->delete();
+
+        return ResponseFormatter::success($token, 'Token Revoked');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 
 Route::get('products', [App\Http\Controllers\API\ProductController::class, 'all']);
 Route::get('categories', [App\Http\Controllers\API\ProductCategoryController::class, 'all']);
 
 Route::post('register', [App\Http\Controllers\API\UserController::class, 'register']);
 Route::post('login', [App\Http\Controllers\API\UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
+    Route::post('logout', [UserController::class, 'logout']);
+});
